a6d20deb7650633eaf8feea30d39da484d209e2b
Shubham Kesharvani <94446874+Kesharvani@users.noreply.github.com>
2021-12-18 01:00:03 +0530

    feat:input_output

---
+++ banana.js
@@ -0,0 +1,11 @@
+var listenerr=document.querySelector("#btn");
+var inputselector=document.querySelector("#ipt");
+var outputselector=document.querySelector("#output");
+function translateintobanana(){
+
+    outputselector.innerText="jvyicy "+inputselector.value;
+};
+
+
+listenerr.addEventListener("click" , translateintobanana);
+
