6c2147fe53393288828c8284ecbeade8d112b0ef
Shubham Kesharvani <94446874+Kesharvani@users.noreply.github.com>
2021-12-19 14:38:10 +0530

    feat:API added

--- banana.js
+++ banana.js
@@ -1,10 +1,21 @@
 var listenerr=document.querySelector("#btn");
 var inputselector=document.querySelector("#ipt");
 var outputselector=document.querySelector("#output");
-function translateintobanana(){
 
-    outputselector.innerText="jvyicy "+inputselector.value;
-};
+var serverURL="https://api.funtranslations.com/translate/minion.json";
+function getURL(text){
+
+    return serverURL+"?"+"text="+text;
+}
+function translateintobanana(){
+var inputText=inputselector.value;
+
+fetch(getURL(inputText))
+.then(response=>response.json())
+.then(out=>{
+    var transfer = out.contents.translated
+    outputselector.innerText=transfer})
+}
 
 
 listenerr.addEventListener("click" , translateintobanana);
